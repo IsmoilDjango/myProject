bd3dc12344edc0b2a9543591f7573c473446b0a5
Ismoil11012000 <ismoilwebdeveloper@gmail.com>
2025-02-03 01:32:05 +0500

    Url manzili o'zgarishi

--- my_app/urls.py
+++ my_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from rest_framework import routers
 from my_app.views import PostListCreateAPIView, PostDetailApiView, CommentCreateApiView
 urlpatterns = [
-    path('posts/', PostListCreateAPIView.as_view(), name='list-create'),
-    path('posts/<int:pk>/', PostDetailApiView.as_view(), name='post-detail'),
+    path('api/posts/', PostListCreateAPIView.as_view(), name='list-create'),
+    path('api/posts/<int:pk>/', PostDetailApiView.as_view(), name='post-detail'),
     path('comment/', CommentCreateApiView.as_view(), name='comment')
 
 ]
