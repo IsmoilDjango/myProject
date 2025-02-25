544e486462e2efc3fc649a6ea37196935323e9d3
Ismoil11012000 <ismoilwebdeveloper@gmail.com>
2025-02-25 21:19:55 +0500

    settings.py ALLOWED_HOSTS

--- my_site/settings.py
+++ my_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-_!*688f*8^v+!w@hq%6(2y#i=8ln$g!wnofgfd+mj-9&*j^p4q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myproject-5wxp.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
