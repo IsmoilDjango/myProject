ec534f09f5daa0e927da8f6121877b229f99b8e5
Ismoil11012000 <ismoilwebdeveloper@gmail.com>
2025-02-27 20:17:26 +0500

    update ststicfiles

--- my_site/settings.py
+++ my_site/settings.py
@@ -68,6 +68,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 
 ]
 CORS_ALLOW_ALL_ORIGINS = True
@@ -139,12 +140,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# settings.py
 
+STATIC_URL = '/static/'
+# Statik fayllar Render serveriga ko'chiriladigan joy
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Agar qo'shimcha static papkalar mavjud bo'lsa, ularni ham ko'rsating
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
