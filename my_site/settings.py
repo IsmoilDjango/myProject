cf0dc9370744b14a021199493098fa55e8439d4f
Ismoil11012000 <ismoilwebdeveloper@gmail.com>
2025-02-27 20:32:30 +0500

    Added create_admin.py script and created superuser

--- my_site/settings.py
+++ my_site/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'rest_framework_simplejwt',
+    'django_extensions',
 ]
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
