a8cdc866ff1855dcc67d8c7fb401f780ffa2bb49
Ismoil11012000 <ismoilwebdeveloper@gmail.com>
2025-03-11 07:21:48 +0500

    models.py update

--- my_app/models.py
+++ my_app/models.py
@@ -6,7 +6,8 @@ from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
     def __str__(self):
         return self.name
 class Tag(models.Model):
